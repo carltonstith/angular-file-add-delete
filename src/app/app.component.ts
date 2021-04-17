da6234c71a4b3de4aca874875e7a59971f34ca16
Carlton Stith <carlton215@gmail.com>
2021-04-17 16:44:56 -0400

    files updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-file-add-delete';
+  imageDeleteFrom: FormGroup;
+  imageurls = [];
+  base64String: string;
+  name: string;
+  imagePath: string;
+
+  removeImageEdit(i, imagepath) {
+    this.imageDeleteFrom.value.id = i;
+    this.imageDeleteFrom.value.ImagePath = imagepath;
+  }
+
+  removeImage(i) {
+    this.imageurls.splice(i, 1);
+  }
+
+  onSelectFile(event) {
+    if (event.target.files && event.target.files[0]) {
+      var filesAmount = event.target.files.length;
+      for (let i = 0; i < filesAmount; i++) {
+        var reader = new FileReader();
+        reader.onload = (event: any) => {
+          this.imageurls.push({ base64String: event.target.result });
+        };
+        reader.readAsDataURL(event.target.files[i]);
+      }
+    }
+  }
 }
